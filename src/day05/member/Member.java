58ca648dbf191c0307924e861467133f91807a2c
HongChaa <126743377+HongChaa@users.noreply.github.com>
2023-04-11 12:11:48 +0900

    # filter, map 2023-04-11

--- src/day05/member/Member.java
+++ src/day05/member/Member.java
@@ -1,15 +1,20 @@
 package day05.member;
 
+import java.time.LocalDate;
+
 // 역할: 1명의 회원정보를 묶어주는 역할
 // Java Bean
 public class Member {
 
-    int memberId;
-    String email;
-    String password;
-    String memberName;
-    Gender gender;
-    int age;
+    public int memberId;
+    public String email;
+    public String password;
+    public String memberName;
+    public Gender gender;
+    public int age;
+
+    // 회원가입일
+    LocalDate regDate;
 
     public Member(int memberId, String email, String password, String memberName, Gender gender, int age) {
         this.memberId = memberId;
@@ -21,7 +26,7 @@ public class Member {
     }
 
     // 회원정보를 문자열로 만들어서 리턴하는 기능
-    String inform() {
+    public String inform() {
         String convertGender = (gender == Gender.MALE) ? "남성" : "여성";
 
         return String.format("# %d번 | %s | %s | %s | %d세"
@@ -29,4 +34,6 @@ public class Member {
                 , convertGender, this.age
         );
     }
+
+
 }
--- src/day05/member/MemberRepository.java
+++ src/day05/member/MemberRepository.java
@@ -1,22 +1,31 @@
 package day05.member;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+
 // 역할: 회원 저장소 역할
 public class MemberRepository {
 
     public static final int NOT_FOUND = -1;
+
+    // 가입된 회원 배열
     Member[] memberList;
 
+    // 삭제된 회원 배열
+    Member[] removeMembers;
+
     public MemberRepository() {
-        this.memberList = new Member[3];
-        memberList[0] = new Member(1, "abc@def.com",
-                "1234", "콩벌레",
-                Gender.MALE, 50);
-        memberList[1] = new Member(2, "xxx@zzz.com",
-                "9999", "팥죽이",
-                Gender.FEMALE, 30);
-        memberList[2] = new Member(3, "hhh@qwe.com",
-                "5678", "카레왕",
-                Gender.FEMALE, 44);
+        this.memberList = new Member[0];
+//        memberList[0] = new Member(1, "abc@def.com",
+//                "1234", "콩벌레",
+//                Gender.MALE, 50);
+//        memberList[1] = new Member(2, "xxx@zzz.com",
+//                "9999", "팥죽이",
+//                Gender.FEMALE, 30);
+//        memberList[2] = new Member(3, "hhh@qwe.com",
+//                "5678", "카레왕",
+//                Gender.FEMALE, 44);
     }
 
     /**
@@ -47,8 +56,29 @@ public class MemberRepository {
         for (int i = 0; i < memberList.length; i++) {
             temp[i] = memberList[i];
         }
+
+        // 회원가입 시간 등록
+        newMember.regDate = LocalDate.now();
+
         temp[temp.length - 1] = newMember;
         memberList = temp;
+
+        // save파일 생성
+        try (FileWriter fw = new FileWriter("D:/exercise/member.txt")) {
+
+            String saveInfo = "";
+            saveInfo += newMember.memberId;
+            saveInfo += "," + newMember.email;
+            saveInfo += "," + newMember.memberName;
+            saveInfo += "," + newMember.password;
+            saveInfo += "," + newMember.gender;
+            saveInfo += "," + newMember.age;
+
+            fw.append(saveInfo + "\n");
+
+        } catch (IOException e) {
+            System.out.println("파일 저장 실패!");
+        }
 
         return true;
     }
@@ -73,43 +103,39 @@ public class MemberRepository {
         return !isEmpty() ? memberList[memberList.length - 1].memberId : 0;
     }
 
-
-    /*
+    /**
      * 이메일을 통해 특정 회원 객체를 찾아서 반환하는 기능
-     * @param email : 찾고 싶은 회원의 이메일
+     *
      * @return : 찾은 회원의 객체정보, 못찾으면 null반환
-     * */
-
+     * @param1 email : 찾고 싶은 회원의 이메일
+     */
     Member findByEmail(String email) {
-
-        for (int i = 0; i < memberList.length; i++) {
-            if (memberList[i].email.equals(email)) {
-                return memberList[i];
+        for (Member m : memberList) {
+            if (email.equals(m.email)) {
+                return m;
             }
         }
         return null;
     }
-    /*
-    * 이메일을 통해 저장된 회원의 인덱스값을 알아내는 메서드
-    * @param email - 탐색 대상의 이메일
-    * @return : 찾아낸 인덱스, 못찾으면 -1 리턴
-    * */
-    int findIndexByEmail(String email) {
+
+    /**
+     * 이메일을 통해 저장된 회원의 인덱스값을 알아내는 메서드
+     * @param email - 탐색 대상의 이메일
+     * @return : 찾아낸 인덱스, 못찾으면 -1 리턴
+     */
+    private int findIndexByEmail(String email) {
         for (int i = 0; i < memberList.length; i++) {
-            if (memberList[i].email.equals(email)) {
-                return  i;
-            }
+            if (memberList[i].email.equals(email))
+                return i;
         }
         return NOT_FOUND;
     }
 
-
-
-    /*
-    * 비밀번호를 수정하는 기능
-    * @param email : 수정 대상의 이메일
-    * @param newPassword : 변경할 새로운 비밀번호
-    */
+    /**
+     * 비밀 번호를 수정하는 기능
+     * @param email : 수정 대상의 이메일
+     * @param newPassword : 변경할 새로운 비밀번호
+     */
     boolean changePassword(String email, String newPassword) {
 
         // 인덱스 탐색
@@ -118,25 +144,25 @@ public class MemberRepository {
         // 수정진행
         if (index == NOT_FOUND) return false;
 
-            memberList[index].password = newPassword;
-            return true;
-
+        memberList[index].password = newPassword;
+        return true;
     }
 
     void removeMember(String email) {
         // 인덱스 찾기
-        int foundIndex = findIndexByEmail(email);
+        int delIndex = findIndexByEmail(email);
+
         // 앞으로 땡기기
-        Member[] temp = new Member[memberList.length - 1];
-        for (int i = foundIndex; i < temp.length; i++) {
-            temp[i] = memberList[i + 1];
+        for (int i = delIndex; i < memberList.length - 1; i++) {
+            memberList[i] = memberList[i + 1];
         }
-        for (int i = 0; i < foundIndex; i++) {
+
+        // 배열 마지막 칸 없애기
+        Member[] temp = new Member[memberList.length - 1];
+        for (int i = 0; i < temp.length; i++) {
             temp[i] = memberList[i];
         }
         memberList = temp;
-        temp = null;
-
     }
 
     // 멤버가 비었는지 확인
@@ -144,16 +170,4 @@ public class MemberRepository {
         return memberList.length == 0;
     }
 
-
-
 }
-
-
-
-
-
-
-
-
-
-
